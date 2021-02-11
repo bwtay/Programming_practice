33b98da7853a03c68f555cc4bb0e9f6c05ab9473
bwtay <bwtay@users.noreply.github.com>
2021-02-11 11:16:44 +0800

    Create git_learn_webui_edit.py
    Practice for adding a python script through github's web editor

---
+++ git_learn_webui_edit.py
@@ -0,0 +1,4 @@
+#practice for adding a new file through GitHub's web interface
+#note the .py extension automatically enables syntax highlighting, it's not done through markdown ```python
+
+print("Hello world!")
